1f5f845753f0a5670d5cc6653940a93eb7f5fc71
apgallego <apgallego@users.noreply.github.com>
2022-02-27 15:12:53 +0100

    seen messages +

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -66,27 +66,82 @@ class MessageController extends AbstractController
         ]);
     }
 
+    #[Route('/various-message', name: 'various_message')]
+    public function variousMessage(ManagerRegistry $doctrine, UserRepository $userRepository): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        //Current date for the message
+        /** 
+         * @var \App\Entity\User $user
+         */
+        $user = $this->getUser();
+        $userID = $user->getId();
+        $date = new \DateTime('@' . strtotime('now'));
+        // var_dump($date);
+
+        $message = new Message();
+        $users = $userRepository->findAll();
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            
+            //no possibility of sending messages to your own
+            if ($user->getId() == $_POST['receiver']) {
+                return $this->redirectToRoute('messages_error?errorMessage=Cant send this message');
+            }
+
+            //DATA FROM FORM
+            // if(isset($_POST['receiver']) && $_POST['receiver'] !== '')
+            //     $receiver = $userRepository->findBy(['email' => $_POST['receiver']]);
+            for($i = 0; $i <= count($_POST['receiver']); $i++){
+                //check this
+                $receiver = $userRepository->findBy(['email' => $_POST['receiver'][$i]]);
+                $message->setReceiverID($receiver[0]->getId());
+                $message->setHeader($_POST['messageHead']);
+                $message->setBody($_POST['messageBody']);
+                // $message->setAttachFile($_POST['fileToUpload']);
+
+                //Data that is default
+                $message->setSenderID($user->getId());
+                $message->setTimestamp($date);
+                $message->setIsRead(0);
+                // $message->setIsRead(false);
+
+                //Save new message in database
+                $entityManager->persist($message);
+                $entityManager->flush();
+            }
+
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('message/message.html.twig', [
+            'controller_name' => 'MessageController',
+            // 'userID' => $userID,
+            'users' => $users
+        ]);
+    }
+
     #[Route('/view-message', name: 'view_message')]
-    public function viewMessage(UserRepository $userRepository, MessageRepository $messageRepository): Response
+    public function viewMessage(ManagerRegistry $doctrine, UserRepository $userRepository, MessageRepository $messageRepository): Response
     {
         if(isset($_GET['messageID'])){
             //TODO: fix this and logic here for seen messages
             // --------------------------------------------------
-            $users = $userRepository->findAll();
-            $fullMessage = $messageRepository->findBy(['id' => $_GET['messageID']]);
-            
-            // foreach($users as $user){
-            //     if($user->getId() == $fullMessage[0]->getSenderID()){
-            //         $userPfp = $user->getPfp();
-            //         $userEmail = $user->getEmail();
-            //     }
-            // }
 
+            $users = $userRepository->findAll();
+            $message = $messageRepository->findBy(['id' => $_GET['messageID']]);
+            
+            $entityManager = $doctrine->getManager();
+            
+            $message[0]->setIsRead(1);
+            $entityManager->persist($message[0]);
+            $entityManager->flush();
+            
             return $this->render('message/viewMessage.html.twig', [
                 'controller_name' => 'MessageController',
                 'users' => $users,
                 // 'userPfp' => $userPfp,
-                'message' => $fullMessage[0]
+                'message' => $message[0]
             ]);
         }
     }
